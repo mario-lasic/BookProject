aa09c5d43fde382ebea95176a47dec8213d6d9a8
Mario Lasić <mario.lasic@outlook.com>
2023-03-20 20:10:39 +0100

    reading csv file and making genres and subgenre lists

--- data.py
+++ data.py
@@ -1,13 +1,32 @@
 from vertex import Vertex
 from graph import Graph
-
-types = ["Romance", "Classic", "Fantasy", "Poetry", "Comic", "Mystery", "Horror"]
-
+import csv
+genres = []
+sub_genre = {}
 books = Graph(True)
 
-for type in types:
-    books.add_vertex(Vertex(type, type))
+with open('books.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if row["Genre"] not in genres:
+            genres.append(row["Genre"])
+        list_of_sub = sub_genre.get(row["Genre"])
+        if list_of_sub == None:
+            key = row["Genre"]
+            if sub_genre.get(key) is None:
+                sub_genre[key] = []
+                sub_genre[key].append(row["SubGenre"])
+            else:
+                sub_genre[key].append(row["SubGenre"])
+        elif row["SubGenre"] not in list_of_sub:
+            key = row["Genre"]
+            if sub_genre.get(key) is None:
+                sub_genre[key] = []
+                sub_genre[key].append(row["SubGenre"])
+            else:
+                sub_genre[key].append(row["SubGenre"])
+        
 
-print(books.graph_dict["Fantasy"].type)
-
-books_vertices = []
+#print(genres)
+print(sub_genre.values())
+    
