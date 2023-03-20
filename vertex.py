a06fa6244645b64e4d1f417f2185930e05dbd132
Mario Lasić <mario.lasic@outlook.com>
2023-03-20 18:34:54 +0100

    added vertex class with methods

---
+++ vertex.py
@@ -0,0 +1,8 @@
+class Vertex:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.edges = {}
+
+    def get_edge(self):
+        return list[self.edges.keys()]
+        
