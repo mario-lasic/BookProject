c63a8d0a4a97f069fd955072b8c8e5c87a9e6051
Mario Lasić <mario.lasic@outlook.com>
2023-03-21 18:25:41 +0100

    adding search for correct sub genre

---
+++ bfs.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def adding_books(root_node, sub_genre, genre):
+    path_queue = deque()
+
+    initial_path = [root_node]
+
+    path_queue.appendleft(initial_path)
+
+    while path_queue:
+        current_path = path_queue.pop()
+        current_node = current_path[-1]
+        if current_node.genre == genre or current_node.genre is None:
+            
+            if current_node.sub_genre == sub_genre:
+                return current_node
+        
+            for child in current_node.children:
+                new_path = current_path[:]
+                new_path.append(child)
+                path_queue.appendleft(new_path)
+
+    return None
