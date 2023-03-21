ac08cae5f639e043bb18fe16da16de55e8222dc2
Mario Lasić <mario.lasic@outlook.com>
2023-03-21 18:25:05 +0100

    Added author values

--- tree.py
+++ tree.py
@@ -1,10 +1,11 @@
 from collections import deque
 class TreeNode:
 
-    def __init__(self, value = None, genre = None, sub_genre = None) -> None:
-        self.value = value
+    def __init__(self, name = None, genre = None, sub_genre = None, author = None) -> None:
+        self.name = name
         self.genre = genre
         self.sub_genre = sub_genre
+        self.author = author
         self.children = []
 
     def add_child(self, child_node):
@@ -14,9 +15,9 @@ class TreeNode:
         nodes_to_visit = [self]
         while len(nodes_to_visit) > 0:
             current_node = nodes_to_visit.pop()
-            print(current_node.value)
+            print(current_node.name)
             nodes_to_visit += current_node.children
-
+# Tree Visualization helper ! 
     def __str__(self):
         stack = deque()
         stack.append([self, 0])
@@ -26,7 +27,7 @@ class TreeNode:
       
             if level > 0:
                 level_str += "| "*(level-1)+ "|-"
-            level_str += str(node.value)
+            level_str += str(node.name)
             level_str += "\n"
             level+=1
             for child in reversed(node.children):
