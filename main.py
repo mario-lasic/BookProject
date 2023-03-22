f91edb59b8fc4ce461e458e3877e600216bcc6b8
Mario Lasić <mario.lasic@outlook.com>
2023-03-22 20:14:06 +0100

    Added main program

---
+++ main.py
@@ -0,0 +1,10 @@
+from book import startup, recomedations, repeat
+
+startup()
+recomend = True
+
+while recomend:
+    recomedations()
+    recomend = repeat()
+
+print("\n Thank you for using my program GoodBay! ")
